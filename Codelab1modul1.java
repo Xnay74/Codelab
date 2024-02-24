6468e2d5c2e0e6103a75887cae3063413940b55f
Xnay74 <muhammadrinasyah475@gmail.com>
2024-02-24 13:24:39 +0700

    first commit

---
+++ Codelab1modul1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner ;
+import java.time.LocalDate;
+import java.time.Period ;
+
+public class Codelab1modul1 {
+    public static void main(String[] args) {
+        Scanner kata = new Scanner(System.in);
+
+        System.out.print("Nama :");
+        String Nama = kata.nextLine();
+
+        System.out.print("Jenis kelamin (L\\P) :");
+        char Jeniskelamin = kata.next().charAt(0);
+
+        System.out.print("Tanggal Lahir (yyyy-mm-dd) :");
+        String TanggalLahirStr = kata.next();
+
+        LocalDate TanggalLahir = LocalDate.parse(TanggalLahirStr);
+
+        LocalDate Sekarang = LocalDate.now();
+        Period Umur = Period.between(TanggalLahir, Sekarang);
+
+        System.out.println("\nNama :" +Nama);
+        System.out.println("Jenis Kelamin :" + (Jeniskelamin == 'L' ? "Laki-laki" : "perempuan"));
+        System.out.println("Umur :" + Umur.getYears() + " Tahun " + Umur.getMonths() + " Bulan " + Umur.getDays() + " hari ");
+
+    }
+}
